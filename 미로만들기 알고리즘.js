9545f1b8e93098ea6d365e9118668231c4d7fd71
chairking <39178226+hyunbeanohh@users.noreply.github.com>
2020-07-15 19:03:29 +0900

    Update 미로만들기 알고리즘.js

--- 미로만들기 알고리즘.js
+++ 미로만들기 알고리즘.js
@@ -1,7 +1,9 @@
+console.log('이동 전 지도');
 
-
-function make_map(row,colum,charloc,block){
+function make_map(row,colum,charloc,block,moving){
   let map = [];
+
+
   for(let i =0 ; i<colum+2; i++){
     map.push(Array(row+2).fill(0));
   }
@@ -25,7 +27,34 @@ function make_map(row,colum,charloc,block){
   for(let i of map){
     console.log(i);
   }
+
+  let movingchar = [charloc[0]+1,charloc[1]+1];
+
+  let x = movingchar[1];
+  let y = movingchar[0];
+
+  for(let i of moving){
+    if(i === 2 && map[y+1][x] !== 2){
+      y += 1;
+    }
+    else if(i === 4 && map[y][x+1] !== 2){
+      x += 1;
+    }
+  }
+
+  map[y][x] =1;
+
+console.log('이동 후 지도');
+  for(let i of map){
+    console.log(i);
+  }
+
   return map;
 }
 
-make_map(4, 5, [0, 0], [[0,1],[1,1],[2,3],[1,3]]);
+make_map(4, 5, [0, 0], [[0,1],[1,1],[2,3],[1,3]],[2,2,2,4,4,4]);
+
+
+
+
+

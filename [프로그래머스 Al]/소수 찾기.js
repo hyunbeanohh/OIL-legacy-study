970d8f2c186c7866d64f5da5d24dc7f70511fa37
chairking <39178226+hyunbeanohh@users.noreply.github.com>
2020-07-23 16:54:06 +0900

    소수 찾기.js
    에라토스테네스의 체를 사용하지 않고 풀어봤더니 테스트 케이스 몇개에서 실패가 떴고 , 효율성은 전부 탈락했다 . 그래서 찾아봤는데 에라토스테네스의 체를 사용하면 소수를 찾을 때 제일 성능이 좋은걸 알 수 있어서 적용해서 풀었더니 통과했다.

---
+++ [프로그래머스 Al]/소수 찾기.js
@@ -0,0 +1,24 @@
+function solution(n){
+ 
+  const arr = [] ;
+  
+  for(let i = 0; i< n+1; i+=1){
+    arr.push(true);
+  }
+
+  for(let i = 2 ;i * i <=n; i+=1){
+    if(arr[i]){
+      for(let j = i*i; j<=n ; j+=i){
+        arr[j] = false;
+      }
+    }    
+  }
+  arr.splice(0,2,false,false);
+
+  const result = arr.filter((x)=>{
+    return x !== false;
+  })
+  return result.length;
+}
+
+console.log(solution(10000));

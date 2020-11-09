65f6193454c960d54b7b1a5f8d3973b641f38be6
chairking <39178226+hyunbeanohh@users.noreply.github.com>
2020-11-09 16:38:14 +0900

    Update 구명보트.py

--- [프로그래머스 Al]/구명보트.py
+++ [프로그래머스 Al]/구명보트.py
@@ -11,3 +11,20 @@ def solution(people, limit):
         pcount -= 1
         
     return count
+#순서도
+# 0 <= 3
+# count = 1
+# if [0] + [3] <= 100 = 50 + 80 <= 100 ?
+#아니면 pSize -= 1  3 - 1 = 2
+#0 <= 2
+#count = 2
+# if [0] + [2] <= 100 = 50 + 70 <= 120 ?
+#아니면 pSize -= 1 2 - 1 = 1
+# 0 <= 1
+#count = 3
+#아니면 if [0] + [1] <= 100 = 50 + 50 <= 100?
+# fSize += 1 0 + 1 = 1
+#pSize -= 1 = 1 - 1 = 0
+# 1 <= 0 이므로 while문 종료
+#count = 3
+    

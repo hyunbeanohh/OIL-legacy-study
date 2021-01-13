339150ab566f309fb47d161933fa571d28f61432
chairking <39178226+hyunbeanohh@users.noreply.github.com>
2021-01-13 19:45:11 +0900

    Js solved

---
+++ Hacker Ranks/Breaking the Records.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the breakingRecords function below.
+function breakingRecords(scores) {
+    let maxnum = scores[0]
+    let minnum = scores[0]
+    let cnt = [0,0]
+    for(let i = 0 ; i<scores.length; i++){
+        if (scores[i] >maxnum){
+            maxnum = scores[i]
+            cnt[0]++
+        }
+        if (scores[i] < minnum){
+            minnum = scores[i]
+            cnt[1]++
+        }
+    }
+    return cnt
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const scores = readLine().split(' ').map(scoresTemp => parseInt(scoresTemp, 10));
+
+    const result = breakingRecords(scores);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}

0b378437b7c67b054c42482d1f9c4bc4a5c7eb9c
chairking <39178226+hyunbeanohh@users.noreply.github.com>
2021-01-18 23:05:23 +0900

    python3 solved

---
+++ Hacker Ranks/Piciking Numbers.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    maxcount = 0
+    for i in a:
+        num = a.count(i)
+        num2 = a.count(i-1)
+        num = num+num2
+        if num >maxcount:
+            maxcount = num
+    return maxcount
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

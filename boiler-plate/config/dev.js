980a4ef7d74173d7df2d4329fb67f5544d1438e2
chairking <39178226+hyunbeanohh@users.noreply.github.com>
2021-01-23 00:21:10 +0900

    Delete boiler-plate directory

--- boiler-plate/config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-    mongoURI:'mongodb+srv://chairking95:156423@boiler-plate.7unsd.mongodb.net/boiler-plate?retryWrites=true&w=majority'
-    
-}
--- boiler-plate/config/key.js
+++
@@ -1,6 +0,0 @@
-if(process.env.NODE_ENV=== 'production'){
-    module.exports = require('./prod');
-} //환경변수 설정
-else{
-    module.exports = require('./dev')
-}

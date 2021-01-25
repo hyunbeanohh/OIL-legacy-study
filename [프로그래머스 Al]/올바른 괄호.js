063653f0b42d4ef9081af5a90874d0ab2f4a371d
chairking <39178226+hyunbeanohh@users.noreply.github.com>
2021-01-25 17:30:35 +0900

    Js solved

---
+++ [프로그래머스 Al]/올바른 괄호.js
@@ -0,0 +1,9 @@
+function solution(s){
+    var answer = true;
+    let sum = 0
+    for(let i = 0; i<s.length; i++){
+        s[i] ==='(' ?  sum++ : sum--
+         if(sum < 0) break
+    }
+    return sum === 0;
+}

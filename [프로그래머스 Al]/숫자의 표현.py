1b6693232c97476c4f76127be6415fe3a0a3f5dd
chairking <39178226+hyunbeanohh@users.noreply.github.com>
2021-03-01 23:44:31 +0900

    Rename .py

--- [프로그래머스 Al]/숫자의 표현.py
+++ [프로그래머스 Al]/숫자의 표현.py
@@ -1,4 +1,3 @@
-```
 def solution(n):
     answer = 0
     cnt = 0
@@ -17,4 +16,3 @@ def solution(n):
  ```
  def solution(num):
     return len([i  for i in range(1,num+1,2) if num % i is 0])
- ```

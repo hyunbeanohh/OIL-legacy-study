4815aa54e84cdad0c210e65d09b160ff42b31f3c
chairking <39178226+hyunbeanohh@users.noreply.github.com>
2021-03-10 16:34:58 +0900

    Plus BFS

--- [프로그래머스 Al]/네트워크 .py
+++ [프로그래머스 Al]/네트워크 .py
@@ -15,3 +15,27 @@ def dfs(n,computers,start,visited):
         if connect != start and computers[start][connect] == 1:
             if visited[connect] == False:
                 dfs(n,computers,connect,visited)
+
+                
+#BFS
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
+    for i in range(n):
+        if visited[i] == False:
+            answer += 1
+            bfs(n,computers,i,visited)
+    return answer
+
+
+def bfs(n,computers,start,visited):
+    visited[start] = True
+    q = []
+    q.append(start)
+    while q:
+        start = q.pop()
+        visited[start] = True
+        for connect in range(n):
+            if connect != start and computers[start][connect] == 1:
+                if visited[connect] == False:
+                    q.append(connect)

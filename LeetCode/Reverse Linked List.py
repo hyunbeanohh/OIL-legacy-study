371097faee77dcbb0bf9c1b3a4d7c11e56759f66
chairking <39178226+hyunbeanohh@users.noreply.github.com>
2021-03-26 15:17:59 +0900

    recursive , iterate solv
    반복문이 공간 복잡도를 70%정도 차지해 재귀에 비해 낮은 편

---
+++ LeetCode/Reverse Linked List.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        """
+        def reverse(node:ListNode, prev : ListNode = None):
+            if not node:
+                return prev
+            next,node.next = node.next,prev
+            return reverse(next,node)
+        
+        return reverse(head)""" 
+        
+        
+        node , prev = head , None
+        
+        while node:
+            next,node.next = node.next , prev
+            prev, node = node ,next
+        return prev
+      
+      # 재귀 - 40ms , 반복 - 32ms

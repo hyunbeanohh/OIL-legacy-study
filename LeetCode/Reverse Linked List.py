fa0214c832b9c619ce71a3a9e8c7cf415ce25f51
chairking <39178226+hyunbeanohh@users.noreply.github.com>
2021-04-02 14:43:35 +0900

    Update Reverse Linked List.py

--- LeetCode/Reverse Linked List.py
+++ LeetCode/Reverse Linked List.py
@@ -1,13 +1,12 @@
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        """
         def reverse(node:ListNode, prev : ListNode = None):
             if not node:
                 return prev
             next,node.next = node.next,prev
             return reverse(next,node)
         
-        return reverse(head)""" 
+        return reverse(head)
         
         
         node , prev = head , None

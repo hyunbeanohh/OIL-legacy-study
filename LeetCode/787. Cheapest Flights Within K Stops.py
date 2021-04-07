fca83e78bcab32ae3e8ff6099cae2ddd9648ea84
chairking <39178226+hyunbeanohh@users.noreply.github.com>
2021-04-07 16:44:42 +0900

    최단  경로  알고리즘 (BFS)

---
+++ LeetCode/787. Cheapest Flights Within K Stops.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
+        graph = collections.defaultdict(list)
+        for u,v,w in flights:
+            graph[u].append((v,w))
+        
+        q = [(0,src,K)] # [(가격,정점,남은 가능 경유지 수)]
+        
+        while q: # 우선 순위 큐 최솟값 기준으로  도착점까지 최소 비용 판별
+            price,node,k = heapq.heappop(q)
+            if  node ==  dst:
+                return price
+            if k >= 0:
+                for v,w in graph[node]:
+                    alt =  price+w
+                    heapq.heappush(q,(alt,v,k-1))
+        return  -1

266ee0951a9d2bafe6a589ccb5d882125343fc65
chairking <39178226+hyunbeanohh@users.noreply.github.com>
2021-04-19 00:31:28 +0900

    DFS solve

---
+++ [프로그래머스 Al]/여행경로.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+def solution(tickets):
+    answer = []
+    graph = defaultdict(list)
+    
+    for a,b in sorted(tickets):
+        graph[a].append(b)
+  
+
+    def dfs(a):
+        while graph[a]:
+            dfs(graph[a].pop(0))
+        answer.append(a)
+        
+    dfs('ICN')
+    return answer[::-1]

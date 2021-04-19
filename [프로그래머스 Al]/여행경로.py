1c344f40a80b79085c46583696c71cdda5ec3c79
chairking <39178226+hyunbeanohh@users.noreply.github.com>
2021-04-19 14:23:18 +0900

    그래프 개선을 통한 시간 복잡도 효울 개선

--- [프로그래머스 Al]/여행경로.py
+++ [프로그래머스 Al]/여행경로.py
@@ -14,3 +14,18 @@ def solution(tickets):
         
     dfs('ICN')
     return answer[::-1]
+
+from collections import defaultdict
+def solution(tickets):
+    answer = []
+    graph = defaultdict(list)
+    for a,b in sorted(tickets,reverse=True):
+        graph[a].append(b)
+    
+    def dfs(a):
+        while graph[a]:
+            dfs(graph[a].pop())
+        answer.append(a)
+        
+    dfs('ICN')
+    return answer[::-1]

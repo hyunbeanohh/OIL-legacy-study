a4aaa9f58dcbef98e2bf0cfcc8cd88abfc798bff
chairking95 <ohb4199@naver.com>
2021-05-28 16:40:09 +0900

    타입스크립트 란?, 사용이유, 기본 선언 방식

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,14 @@
+//기본 문자열 JS
+//var str = 'hello';
+
+//TS 문자열 선언
+let str: string = 'hello';
+
+//TS 숫자형 선언
+let num: number = 1;
+
+// TS 배열 선언
+let arr: Array<number> = [1,2,3];
+let heroes: Array<string> = ['hyunbin','suhyun']
+let items:number[] = [1,2,3]
+
---
+++ why-ts/sample.ts
@@ -0,0 +1,6 @@
+function numsum(a: number, b:number) : number{
+    return a+b
+}
+
+var result = numsum(10,20) // 속성을 정해주기때문에 맞는 api를 자동완성을 통해서 바로 불러올 수 있다.
+result.toLocaleString();

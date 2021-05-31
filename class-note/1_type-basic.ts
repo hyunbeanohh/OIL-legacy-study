d187e01d1ec9b0855a329332f3643bf83b5e42d4
chairking95 <ohb4199@naver.com>
2021-05-31 16:07:44 +0900

    기본 타입 튜플,객체,진위값

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -12,3 +12,20 @@ let arr: Array<number> = [1,2,3];
 let heroes: Array<string> = ['hyunbin','suhyun']
 let items:number[] = [1,2,3]
 
+//TS 튜플 배열의 각각 인덱스의 타입이 정해져있다.
+let address: [string,number] = ['gangnam',123]
+
+//TS 객체
+let obj: object = {}
+// let person: object ={
+//     name:'capt',
+//     age:100
+// }
+let person: {name:string, age:number} = {
+    name : 'cpat',
+    age : 100
+}
+
+//TS 진위값
+//let show = true;
+let show: boolean = true
